e3f8880aacd6d68c615f3fdd3dd3b82c1ddeb4cd
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-09 10:42:23 +0600

    Update YouDo7.java
    Create an interface Clickable with a single empty method onClick(). Implement this interface in a Button class, providing the logic to print "Button was clicked!". In Main, create a Button object and call onClick().

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,22 @@
+interface Clickable{
+    void onClick();
+}
 
+class Button implements Clickable{
+    
+    //method
+    public void onClick(){
+        
+        System.out.println("Button was clicked!");
+       
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Button b = new Button();
+        
+        b.onClick();
+    }
+}
