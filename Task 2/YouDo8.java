fb689caa1d91c53436bca752ea50f451d2842960
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-09 10:44:50 +0600

    Update YouDo8.java
    - Item Class: * Add a String name and a double price. 
    - Create a constructor to set these values when the object is created. 
    - Order Class: * Declare two Item variables: item1 and item2. 
    - Create a constructor that takes two Item objects and assigns them. 
    - Create a void printBill() method that adds the prices of both items together and prints the total cost. 
    - Main Class: Create two Item objects (e.g., a "Mouse" for 20.0 and a "Keyboard" for 45.0). 
    - Create one Order object, passing your two items into its constructor. 
    - Call printBill() on the order to see the final output.

--- Task 2/YouDo8.java
+++ Task 2/YouDo8.java
@@ -1,1 +1,45 @@
+class Item{
+    String name;
+    double price;
+    
+    //constructor
+    Item(String name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+}
 
+class Order{
+    Item item1;
+    Item item2;
+    
+    Order(Item item1, Item item2)
+    {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
+    
+    void printBill(){
+    double total = item1.price+item2.price;
+    System.out.println("----- BILL -----");
+    System.out.println(item1.name+": "+item1.price);
+    System.out.println(item2.name+": "+item2.price);
+    System.out.println("Total cost: "+total);
+    
+    }
+}
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Item mouse = new Item("Mouse",20.0);
+        Item kb = new Item("Keyboard",45.0);
+        
+        Order order = new Order(mouse,kb);
+        
+        order.printBill();
+    }
+}
