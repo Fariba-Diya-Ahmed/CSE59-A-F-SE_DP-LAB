004a6f772e6271fc85d4fc69a32736a6c2fa4f7c
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-09 10:40:47 +0600

    Update YouDo5.java
    Create a Printer class. Write one printData(String text) method that prints the text. Write an overloaded printData(int number) method that prints the number. Instantiate Printer in Main and call both methods.

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,24 @@
-
+class Printer{
+    
+    //method 1
+    void printData(String text){
+        
+        System.out.println("Printing text: "+text);
+       
+    }
+    //method 2
+    void printData(int num){
+        
+        System.out.println("Printing number: "+num);
+       
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        
+        Printer p = new Printer();
+        
+        p.printData("Hello World");
+        p.printData(42);
+    }
+}
