ee21ef82e39df2d9e388382bc7f0daa303638640
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-09 10:40:00 +0600

    Update YouDo4.java
    Create a Sensor class with a String name and an int batteryLevel. Add a charge() method that sets the batteryLevel to 100 and prints "Sensor fully charged". In Main, create a Sensor object, call charge(), and print its new battery level to verify.

--- Task 2/YouDo4.java
+++ Task 2/YouDo4.java
@@ -1,1 +1,28 @@
-
+class Sensor{
+    
+    String name;
+    int batteryLevel;
+    
+    //constructor calling
+    Sensor(String name)
+    {
+        this.name = name;
+        this.batteryLevel = 0;
+    }
+    
+    //method 
+    void charge(){
+        
+        batteryLevel = 100;
+        System.out.println("Sensor fully charged!");
+       
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        
+        Sensor s = new Sensor("Sensor");
+        s.charge();
+        System.out.println("Battery level: "+ s.batteryLevel);
+    }
+}
