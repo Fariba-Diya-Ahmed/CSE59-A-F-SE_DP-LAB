b3d898ee6f75692e219675975ddef5b3dba8c8c5
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-23 11:32:15 +0600

    Add MessageSender interface and notification service
    Implemented MessageSender interface with EmailSender and SMSSender classes. Added NotificationService to send alerts using different message types.

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,43 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
 
+class EmailSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+class SMSSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+class NotificationService {
+
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+        NotificationService ns1 = new NotificationService(email);
+        ns1.alertUser("Hello via Email");
+
+        MessageSender sms = new SMSSender();
+        NotificationService ns2 = new NotificationService(sms);
+        ns2.alertUser("Hello via SMS");
+    }
+}
