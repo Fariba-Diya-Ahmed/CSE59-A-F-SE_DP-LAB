8fe162f48c08240076e1480f714173b8559d0a27
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-23 10:58:15 +0600

    Implement printer, scanner, and fax interfaces

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,51 @@
+import java.util.*;
 
+interface Printer {
+    void printDocument();
+}
+
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+class BasicPrinter implements Printer{
+  public void printDocument()
+  {
+    System.out.println("Printing");
+  }
+}
+
+class MultifunctionCopier implements Printer,Scanner,FaxMachine{
+  public void printDocument()
+  {
+    System.out.println("Printing");
+  }
+
+  public void scanDocument()
+  {
+    System.out.println("Scanning");
+  }
+
+  public void faxDocument()
+  {
+    System.out.println("Faxing");
+  }
+}
+
+public class Main3 {
+    public static void main(String[] args) {
+
+      Printer p = new BasicPrinter();
+      p.printDocument();
+
+      MultifunctionCopier m = new MultifunctionCopier();
+
+      m.printDocument();
+      m.scanDocument();
+      m.faxDocument();
+    }
+}
