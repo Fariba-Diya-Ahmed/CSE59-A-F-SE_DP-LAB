4c4d2afc07ee1a8bbd873478fa6c1e0041b52517
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-23 10:40:44 +0600

    Implement TeamMember and BonusEligible interfaces
    Added interfaces for team members and bonus eligibility, implemented in Employee and Contractor classes.

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,47 @@
+import java.util.*;
 
+interface TeamMember{
+    String getName();
+}
+
+interface BonusEligible{
+    void calculateBonus();
+}
+
+class Employee implements TeamMember, BonusEligible {
+  private String name;
+
+  public Employee(String name) {
+      this.name = name;
+  }
+    
+  public String getName(){
+    return name;
+  }
+
+  public void calculateBonus(){
+    System.out.println("Calculating standard employee bonus...");
+  }
+}
+
+class Contractor implements TeamMember{
+  private String name;
+
+  public Contractor(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+}
+  
+public class Main {
+    public static void main(String[] args) {
+      TeamMember e1 = new Employee("Fariba Diya Ahmed");
+      TeamMember c1 = new Contractor("Iffath Sanjida");
+
+      BonusEligible emp = new Employee("Fariba Diya Ahmed");
+      emp.calculateBonus();
+    }
+}
