15e166ebcea744812c00ddfc1159aa7328519ecc
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-25 13:07:03 +0600

    Add interfaces and classes for printing, scanning, and faxing

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,51 @@
+interface Printer {
 
+    void printDocument();
+
+}
+
+interface Scanner {
+
+    void scanDocument();
+
+}
+
+interface FaxMachine {
+
+    void faxDocument();
+
+}
+class BasicPrinter implements Printer {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        Printer p = new BasicPrinter();
+        p.printDocument();
+
+        MultiFunctionCopier mfc = new MultiFunctionCopier();
+
+        mfc.printDocument();
+        mfc.scanDocument();
+        mfc.faxDocument();
+    }
+}
