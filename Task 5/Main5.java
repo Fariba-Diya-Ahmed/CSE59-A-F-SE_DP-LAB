ba6c631ce578a18f8a6e2668a95d9ab04e072288
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-30 11:07:54 +0600

    Add UniversityMember and Researcher classes

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,42 @@
+class UniversityMember {
+    public void attendLecture(){
+        System.out.println("Attending a lecture.");
+    }
+}
 
+
+interface Researcher {
+    void conductLabResearch();
+}
+
+class Professor extends UniversityMember implements Researcher{
+    public void conductLabResearch(){
+        System.out.println("ProfessorConducting lab research.");
+    }
+}
+
+class GRA extends UniversityMember implements Researcher{
+    public void conductLabResearch(){
+        System.out.println("GRA Conducting lab research.");
+    }
+}
+
+class UndergraduateStudent extends UniversityMember {}
+
+public class Main5 {
+
+    public static void main(String[] args) {
+
+        UndergraduateStudent s = new UndergraduateStudent();
+        s.attendLecture();
+
+        Professor p = new Professor();
+        p.attendLecture();
+        p.conductLabResearch();
+
+        GRA gra = new GRA();
+
+        gra.attendLecture();
+        gra.conductLabResearch();
+    }
+}
