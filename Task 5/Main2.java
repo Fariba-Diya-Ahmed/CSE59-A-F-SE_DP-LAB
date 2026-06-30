1dadbafd65b3037887aad33f8179db18164896f0
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-30 10:19:44 +0600

    Implement message sending via Email and SMS

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,41 @@
+interface messageSender{
+    void sendMessage(String msg);
+}
 
+class EmailSender implements messageSender{
+    public void sendMessage(String msg){
+        System.out.println("Sending Email: " + msg);
+    }
+}
+
+class SMSSender implements messageSender{
+    public void sendMessage(String msg){
+        System.out.println("Sending SMS: "+ msg);
+    }
+}
+
+class NotificationService{
+    private messageSender s;
+
+    public NotificationService(messageSender s){
+        this.s = s;
+    }
+
+    public void alterUser(String msg){
+        s.sendMessage(msg);
+    }
+}
+
+public class Main2{
+    public static void main(String[] args) {
+        messageSender email = new EmailSender();
+
+        NotificationService notif1 = new NotificationService(email);
+        notif1.alterUser("Notification via Email");
+
+        messageSender sms = new SMSSender();
+
+        NotificationService notif2 = new NotificationService(sms);
+        notif2.alterUser("Notification via SMS");
+    }
+}
