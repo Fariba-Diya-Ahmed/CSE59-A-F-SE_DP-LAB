257c82fe1b0d2c028149f16c969577b4998f9070
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-30 10:55:25 +0600

    Implement quantization for ResNet18 and MobileNet
    Added OptimizableModel interface and implementations for ResNet18 and MobileNet with quantization methods.

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,34 @@
+interface OptimizableModel{
+    void quantize();
+}
 
+class ResNet18 implements OptimizableModel{
+    public void quantize(){
+        System.out.println("Applying quantization to ResNet18 model");
+    }
+}
+
+class MobileNet implements OptimizableModel{
+    public void quantize(){
+        System.out.println("Applying dynamic quantization to MobileNet model");
+    }
+}
+
+class InferenceEngine{
+    public void quantizeModel(OptimizableModel model){
+        model.quantize();
+    }
+}
+
+public class Main4 {
+    public static void main(String args[]){
+        InferenceEngine engine = new InferenceEngine();
+
+        ResNet18 resnet = new ResNet18();
+
+        MobileNet m = new MobileNet();
+
+        engine.quantizeModel(resnet);
+        engine.quantizeModel(m);
+    }
+}
