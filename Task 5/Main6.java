5a6f45e0cb91e7330c9b06d2008048f725a53e85
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-30 11:12:52 +0600

    Add interfaces and implementations for services

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,55 @@
+interface HttpService {
+    void handleHttpRequest();
+}
 
+
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+
+class WebController implements HttpService {
+    public void handleHttpRequest() {
+
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+
+class DatabaseService implements DatabaseManager {
+
+    public void executeSqlStatement() {
+        System.out.println("Executing SQL query.");
+    }
+}
+
+class DockerManager implements ContainerOps {
+
+    public void restartDockerContainer() {
+        System.out.println("Restarting Docker container.");
+    }
+}
+
+
+public class Main6 {
+    public static void main(String[] args) {
+
+        WebController c1 =new WebController();
+
+        c1.handleHttpRequest();
+
+
+        DatabaseService database = new DatabaseService();
+
+        database.executeSqlStatement();
+
+        DockerManager docker = new DockerManager();
+
+        docker.restartDockerContainer();
+    }
+}
