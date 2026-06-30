470b3500d8416b7cef35d3e3956831cdb22d1c02
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-30 10:07:36 +0600

    Implement multi-function copier and basic printer

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,45 @@
+interface printer {
+    void printDocument();
+}
 
+interface scanner {
+    void scanDocument();
+}
+
+interface faxMachine {
+    void faxDocument();
+}
+
+class multiFunctionCopier implements printer,scanner,faxMachine{
+    public void printDocument(){
+        System.out.println("Printing");
+    }
+
+    public void scanDocument(){
+        System.out.println("Scanning");
+    }
+
+    public void faxDocument(){
+        System.out.println("Faxing");
+    }
+}
+
+class basicPrinter implements printer{
+    public void printDocument(){
+        System.out.println("Printing");
+    }
+}
+
+public class Main1{
+    public static void main(String[] args) {
+        multiFunctionCopier c = new multiFunctionCopier();
+
+        c.printDocument();
+        c.scanDocument();
+        c.faxDocument();
+
+        basicPrinter p = new basicPrinter();
+
+        p.printDocument();
+    }
+}
