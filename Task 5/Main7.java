c5ed5e2a4e9a4dec8e1874431b38fde577c1a2f8
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-06-30 11:24:58 +0600

    Implement disaster alert system with network protocols

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,42 @@
+interface INetworkProtocol{
+    void broadcastMessage(String msg);
+}
+
+class BluetoothMeshNetwork implements INetworkProtocol{
+    public void broadcastMessage(String msg){
+        System.out.println("Broadcasting via bluetooth: " + msg);
+    }
+}
+
+class WiFiDirectNetwork implements INetworkProtocol{
+    public void broadcastMessage(String msg){
+        System.out.println("Broadcasting via WiFi Direct: " + msg);
+    }
+}
+
+class DisasterAlertSystem{
+    private INetworkProtocol network;
+    public DisasterAlertSystem(INetworkProtocol network){
+        this.network = network;
+    }
+    public void triggerAlert(String alertData){
+    network.broadcastMessage(alertData);
+    }
+}
+
+public class Main7{
+    public static void main(String[] args) {
+        INetworkProtocol bluetooth = new BluetoothMeshNetwork();
+
+        DisasterAlertSystem alert1= new DisasterAlertSystem(bluetooth);
+        alert1.triggerAlert("alert via bluetooth");
+
+        INetworkProtocol wifi = new WiFiDirectNetwork();
+        DisasterAlertSystem alert2 = new DisasterAlertSystem(wifi);
+
+        alert2.triggerAlert("alert via wifi");
+    }
+}
+
+
 
